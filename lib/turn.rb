6ebb559daff48fc198115afe9818291ec2ef0e3d
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-01 19:51:04 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,51 @@
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+
+# code your input_to_index and move method here!
+
+  def turn(board)
+  counter = 0
+  while counter < 9
+  puts "Please enter 1-9:"
+  
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
+  
+def input_to_index(input) 
+  new_input = input.to_i - 1
+  return new_input
+end
+
+  def valid_move?(board, index)
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      return false
+    else
+      return true
+    end
+
+
+  def on_board?(number)
+    if number.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  
+end
+counter += 1
+end
