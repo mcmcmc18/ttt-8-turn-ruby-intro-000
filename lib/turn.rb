2933bf728da29c85b6cafef647d0f914b9da9c4f
MC Simoes <mariacristina.simoes@gmail.com>
2018-11-01 20:01:33 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,7 +8,10 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-
+def input_to_index(input) 
+  new_input = input.to_i - 1
+  return new_input
+end
   
 def move(board, index, character = "X")
   board[index] = character
@@ -38,11 +41,6 @@ end
   else
     return false
   end
-end
-
-def input_to_index(input) 
-  new_input = input.to_i - 1
-  return new_input
 end
 
 def turn (board)
